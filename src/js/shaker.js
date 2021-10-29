73c1d98bf02fb025f8d492ceb44f167c4db1e0e2
Smolldo <maximqwerbich@gmail.com>
2021-10-29 19:22:00 +0300

    try shaker

---
+++ src/js/shaker.js
@@ -0,0 +1,11 @@
+var myShakeEvent = new Shake({
+    threshold:15,
+    timeout:1000
+})
+
+myShakeEvent.start();
+
+window.addEventListener('shake', shakeEventDidOccur, false);
+const shakeEventDidOccur = () =>{
+    alert('shaked')
+}
